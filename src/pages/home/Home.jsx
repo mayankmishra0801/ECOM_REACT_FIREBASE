a7726cffc9edb726ebd4fae91579bee6bd868dc1
mayankmishra0801 <mayank0801mishra@gmail.com>
2025-02-08 17:54:58 +0530

    footer,testimonial

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,9 @@ import React, { useContext } from 'react'
 import Layout from '../../components/layout/layout'
 import myContext from '../../context/data/myContext';
 import HeroSection from '../../components/heroSection/HeroSection';
+import Filter from '../../components/filter/Filter';
+// import ProductCard from '../../components/productCard/ProductCard';
+import Testimonial from '../../components/testimonial/Testimonial';
 const Home = () => {
     const context = useContext(myContext);
     console.log(context)
@@ -13,7 +16,10 @@ const Home = () => {
     // <div>Home</div>
     <Layout>
         <HeroSection/>
-        
+        <Filter/>
+        {/* <ProductCard/> */}
+        <Testimonial/> 
+
 {/* <h1>Name: {state.name}</h1>
 <h1>Rollno: {state.rollno}</h1> */}
 {/* <h2>Color:{color}</h2> */}
